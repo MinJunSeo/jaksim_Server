5244485117aa1622e48d47d9e22dc9f31fd948db
MinJunSeo <smj0385@naver.com>
2021-03-31 09:48:48 +0900

    feat: post entity

---
+++ src/entity/post.ts
@@ -0,0 +1,31 @@
+import { ObjectType, Field, ID } from "type-graphql";
+import { User } from "./user";
+import { Category } from "./category";
+import { Tag } from "./tag";
+
+@ObjectType()
+export class Post {
+  @Field(type => ID)
+  readonly id!: number;
+
+  @Field()
+  title!: string;
+
+  @Field(type => String, { nullable: true })
+  content!: string | null;
+
+  @Field()
+  image!: string;
+
+  @Field()
+  createdAt!: Date;
+
+  @Field(type => User)
+  user!: User;
+
+  @Field(type => Category, { nullable: true })
+  category!: Category | null;
+
+  @Field(type => [Tag], { nullable: true })
+  tags!: [Tag] | null;
+}

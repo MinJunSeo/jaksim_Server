854418dc2a75aadcb5fcfc587e6ecad10427560a
MinJunSeo <smj0385@naver.com>
2021-03-31 19:03:45 +0900

    feat: set env variable

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+config();
+
+export default {
+  JWT_SECERT: process.env.JWT_SECERT || "",
+  SERVER_PORT: process.env.SERVER_PORT || "",
+  PASSWORD_SALT: process.env.PASSWORD_SALT || ""
+}

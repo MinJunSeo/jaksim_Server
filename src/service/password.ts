b3aff203111f83d4932c24912e610daa02f8681f
MinJunSeo <smj0385@naver.com>
2021-04-01 13:59:42 +0900

    style: encryptPassword method

--- src/service/password.ts
+++ src/service/password.ts
@@ -4,7 +4,7 @@ import config from "../config"
 export class PasswordService {
   static encryptPassword(password: string) {
     return crypto
-      .pbkdf2Sync(password, config.PASSWORD_SALT, 101856, 60, "sha512")
+      .pbkdf2Sync(password, config.PASSWORD_SALT, 101856, 44, "sha512")
       .toString("base64");
   }
 }

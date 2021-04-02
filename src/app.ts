99fbc81fcc9bb0bc838c3d8d6e54a9e7a94da715
MinJunSeo <smj0385@naver.com>
2021-04-02 19:00:58 +0900

    feat: error formatting

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
-import { UserResolver } from "./resolver";
 import { ApolloServer } from "apollo-server";
+import { UserResolver } from "./resolver";
 import { context } from "./context";
 import config from "./config";
+import { formatError } from "./formatError";
 
 export const app = async () => {
   const schema = await buildSchema({
-    resolvers: [UserResolver]
+    resolvers: [UserResolver],
   });
 
-  new ApolloServer({ schema, context, debug: false })
+  new ApolloServer({
+    schema,
+    context,
+    debug: false,
+    formatError
+  })
     .listen({ port: config.SERVER_PORT }, () => {
       console.log(
         `Server listening at ${config.SERVER_PORT} port!`

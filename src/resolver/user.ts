6a7380c9b90d0869653c0caa849ce3d1fa96b8b2
MinJunSeo <smj0385@naver.com>
2021-04-02 19:39:03 +0900

    feat: user resolver

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,29 +1,35 @@
-import {
-  Resolver,
-  Mutation,
-  Arg,
-  Ctx
-} from "type-graphql";
+import { Resolver, Mutation, Arg, Query } from "type-graphql";
 import { User } from "../entity";
-import { SignupRequest } from "../dto";
-import { PasswordService } from "../service";
-import { Context } from "context";
+import { SignupRequest, GetOneUserRequest, HttpResponse, GetOneUserResponse } from "../dto";
+import { PasswordService, UserService } from "../service";
+import { UserInputError } from "apollo-server";
 
 @Resolver(User)
 export class UserResolver {
-  @Mutation()
+  @Mutation(() => HttpResponse)
   async signup(
-    @Arg("req") req: SignupRequest,
-    @Ctx() ctx: Context
-  ): Promise<User> {
-    req.password = PasswordService.encryptPassword(req.password);
-    return ctx.prisma.user.create({
-      data: {
-        username: req.username,
-        password: req.password,
-        email: req.email,
-        nickname: req.nickname
-      }
-    });
+    @Arg("data") data: SignupRequest,
+  ): Promise<HttpResponse> {
+    const user = await UserService.getOneUser({ username: data.username });
+    if (user) {
+      throw new UserInputError("User Already Exists", {
+        status: 409
+      });
+    }
+
+    data.password = PasswordService.encryptPassword(data.password);
+    await UserService.signup(data);
+
+    return {
+      message: "User Created",
+      status: 201
+    };
+  }
+  
+  @Query(() => User, { nullable: true })
+  async getOneUser(
+    @Arg("data") data: GetOneUserRequest
+  ): Promise<GetOneUserResponse | null> {
+    return await UserService.getOneUser(data);
   }
 }

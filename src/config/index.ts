1218693895d296f84fac245d4b2914e9549a85cc
MinJunSeo <smj0385@naver.com>
2021-04-03 10:20:52 +0900

    fix: PASSWORD_SORT default value

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ config();
 export default {
   JWT_SECERT: process.env.JWT_SECERT || "",
   SERVER_PORT: process.env.SERVER_PORT || "",
-  PASSWORD_SALT: process.env.PASSWORD_SALT || ""
+  PASSWORD_SALT : process.env.PASSWORD_SALT || 0
 }

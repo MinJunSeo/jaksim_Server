ea4a2a373fd40075f28989acf6a75cad19188a71
MinJunSeo <smj0385@naver.com>
2021-04-03 11:44:35 +0900

    fix: getOneUser resolver return tyep

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -1,8 +1,7 @@
 import { Resolver, Mutation, Arg, Query } from "type-graphql";
 import { User } from "../entity";
 import { SignupRequest, GetOneUserRequest, HttpResponse, GetOneUserResponse } from "../dto";
-import { PasswordService, UserService } from "../service";
-import { UserInputError } from "apollo-server";
+import { UserService } from "../service";
 
 @Resolver(User)
 export class UserResolver {
@@ -10,14 +9,6 @@ export class UserResolver {
   async signup(
     @Arg("data") data: SignupRequest,
   ): Promise<HttpResponse> {
-    const user = await UserService.getOneUser({ username: data.username });
-    if (user) {
-      throw new UserInputError("User Already Exists", {
-        status: 409
-      });
-    }
-
-    data.password = PasswordService.encryptPassword(data.password);
     await UserService.signup(data);
 
     return {
@@ -26,7 +17,7 @@ export class UserResolver {
     };
   }
   
-  @Query(() => User, { nullable: true })
+  @Query(() => GetOneUserResponse, { nullable: true })
   async getOneUser(
     @Arg("data") data: GetOneUserRequest
   ): Promise<GetOneUserResponse | null> {

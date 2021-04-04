9a04c051b06b11621909205a666243744312377b
MinJunSeo <smj0385@naver.com>
2021-04-05 08:56:47 +0900

    refactor: file rename

--- src/dto/response/index.ts
+++ src/dto/response/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-export * from "./httpResponse";
-export * from "./getOneUser";
-=======
 export * from "./http";
 export * from "./user";
->>>>>>> 344cb76c8003412ef7a37a41e59bc18ae4ba8297

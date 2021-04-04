bd09783b773e86588c484b23c88979aa3e0221d6
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-04 20:37:34 +0900

    fix: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  JWT_SECERT: process.env.JWT_SECERT || "",
+  JWT_SECRET: process.env.JWT_SECRET || "",
   SERVER_PORT: process.env.SERVER_PORT || "",
-  PASSWORD_SALT: process.env.PASSWORD_SALT || ""
-}
+  PASSWORD_SALT: process.env.PASSWORD_SALT || "",
+};

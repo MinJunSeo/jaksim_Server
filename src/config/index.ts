4713df35cd0ab53a929413ffb0298e2b376332e5
MinJunSeo <smj0385@naver.com>
2021-04-04 19:28:51 +0900

    feat: email server configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,8 @@ config();
 export default {
   JWT_SECERT: process.env.JWT_SECERT || "",
   SERVER_PORT: process.env.SERVER_PORT || "",
-  PASSWORD_SALT : process.env.PASSWORD_SALT || 0
+  PASSWORD_SALT : process.env.PASSWORD_SALT || 0,
+  EMAIL_USER: process.env.EMAIL_USER || "",
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "",
+  EMAIL: process.env.EMAIL || ""
 }

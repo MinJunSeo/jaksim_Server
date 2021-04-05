a91eedf22b2890c980bb2e501eaeb40b626fd010
MinJunSeo <smj0385@naver.com>
2021-04-05 15:47:20 +0900

    refactor: remove nouse env variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ config();
 export default {
   JWT_SECRET: process.env.JWT_SECRET || "",
   SERVER_PORT: process.env.SERVER_PORT || "",
-  PASSWORD_SALT : process.env.PASSWORD_SALT || 0,
   EMAIL_USER: process.env.EMAIL_USER || "",
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "",
   EMAIL: process.env.EMAIL || ""

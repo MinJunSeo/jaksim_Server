d810da37b812b5ff35152e1ae9bfd92ae972bd96
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-06 11:48:35 +0900

    feat: login

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,7 +1,11 @@
-import { SignupRequest, UserResponse } from "../dto";
+import { LoginRequest, SignupRequest, UserResponse } from "../dto";
 import { UserRepository } from "../repository";
 import { UserInputError } from "apollo-server";
 import { PasswordService } from "./password";
+import { UnauthorizedError } from "type-graphql";
+import { LoginResponse } from "../dto/response/login";
+import { JwtGenerator } from "../util/jwtGenerator";
+import { TokenRepository } from "../repository/token";
 
 export class UserService {
   static async signup(data: SignupRequest): Promise<void> {
@@ -19,5 +23,32 @@ export class UserService {
   static async getOneUser(username: string): Promise<UserResponse | null> {
     const user = await UserRepository.findByUsername(username);
     return user ? UserResponse.from(user) : null;
+  }
+
+  static async login({
+    username,
+    password,
+  }: LoginRequest): Promise<LoginResponse> {
+    const user = await UserRepository.findByUsername(username);
+    if (!user) {
+      throw new UnauthorizedError();
+    }
+
+    const isPasswordMatched = await PasswordService.match(
+      password,
+      user.password
+    );
+    if (!isPasswordMatched) {
+      throw new UnauthorizedError();
+    }
+
+    const accessToken = JwtGenerator.accessToken({ username });
+    const refreshToken = JwtGenerator.refreshToken();
+    await TokenRepository.saveRefreshToken(username, refreshToken);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }

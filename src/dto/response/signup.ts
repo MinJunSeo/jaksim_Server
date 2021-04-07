ea305e6eb766dabc6b0f797030e91d19aeb76d53
MinJunSeo <smj0385@naver.com>
2021-04-07 17:10:28 +0900

    feat: result use union type

---
+++ src/dto/response/signup.ts
@@ -0,0 +1,53 @@
+import { Field, ObjectType, createUnionType } from "type-graphql";
+import { IsEmail, Length } from "class-validator";
+import { User } from "../../entity";
+
+@ObjectType()
+export class Signup {
+  constructor(username: string, email: string, nickname: string) {
+    this.username = username;
+    this.email = email;
+    this.nickname = nickname;
+  }
+
+  @Field()
+  @Length(6, 8)
+  username!: string;
+
+  @Field()
+  @IsEmail()
+  email!: string;
+
+  @Field()
+  @Length(2, 5)
+  nickname!: string;
+
+  static from(user: User) {
+    return new Signup(user.username, user.email, user.nickname);
+  }
+}
+
+@ObjectType()
+export class SuccessSignup {
+  constructor() {
+    this.message = "User Created";
+  }
+
+  @Field()
+  message!: string;
+}
+
+@ObjectType()
+export class AlreadyUserExists {
+  constructor() {
+    this.message = "User Already Exists";
+  }
+
+  @Field()
+  message!: string;
+}
+
+export const SignupResponse = createUnionType({
+  name: "SignupResponse",
+  types: () => [SuccessSignup, AlreadyUserExists] as const,
+});

ea5e0ddf07fc90d4564bc1446805242b288495da
MinJunSeo <smj0385@naver.com>
2021-04-08 09:11:45 +0900

    refactor: feature branch

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,18 +1,13 @@
 import {
-  LoginRequest,
   SignupRequest,
-  LoginResponse,
   SignupResult,
-  Signup,
   SuccessSignup,
   AlreadyUserExists,
   EmailVerificationFailed
 } from "../dto";
-import { UserRepository, TokenRepository } from "../repository";
-import { AuthenticationError } from "apollo-server";
+import { UserRepository } from "../repository";
 import { PasswordService } from "./password";
 import { EmailService } from "./email";
-import { JwtGenerator } from "../util/jwtGenerator";
 
 export class UserService {
   static async signup(data: SignupRequest): Promise<typeof SignupResult> {
@@ -29,37 +24,5 @@ export class UserService {
     await UserRepository.save(data.toUserEntity());
 
     return new SuccessSignup();
-  }
-
-  static async getOneUser(username: string): Promise<Signup | null> {
-    const user = await UserRepository.findByUsername(username);
-    return user ? Signup.from(user) : null;
-  }
-
-  static async login({
-    username,
-    password,
-  }: LoginRequest): Promise<LoginResponse> {
-    const user = await UserRepository.findByUsername(username);
-    if (!user) {
-      throw new AuthenticationError("Login Failed");
-    }
-
-    const isPasswordMatched = await PasswordService.match(
-      password,
-      user.password
-    );
-    if (!isPasswordMatched) {
-      throw new AuthenticationError("Login Failed");
-    }
-
-    const accessToken = JwtGenerator.accessToken({ username });
-    const refreshToken = JwtGenerator.refreshToken();
-    await TokenRepository.saveRefreshToken(username, refreshToken);
-
-    return {
-      accessToken,
-      refreshToken,
-    };
   }
 }

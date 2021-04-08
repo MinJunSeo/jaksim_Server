9f05326a68a3ee188e18aa441a58857afda92d40
MinJunSeo <smj0385@naver.com>
2021-04-08 16:56:18 +0900

    refactor: sendVerificationEmail method

--- src/resolver/user.ts
+++ src/resolver/user.ts
@@ -2,8 +2,6 @@ import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../entity";
 import {
   HttpResponse,
-  LoginRequest,
-  LoginResponse,
   SignupRequest,
   SendEmailRequest,
   SignupResult
@@ -17,20 +15,10 @@ export class UserResolver {
     return await UserService.signup(data);
   }
 
-  @Query(() => User, { nullable: true })
-  async getOneUser(
-    @Arg("username") username: string
-  ): Promise<User | null> {
-    return null;
-  }
-
   @Mutation(() => HttpResponse)
-  async sendVerificationEmail(@Arg("data") data: SendEmailRequest): Promise<HttpResponse> {
-    return await EmailService.sendVerificationEmail(data);
-  }
-
-  @Mutation(() => LoginResponse)
-  async login(@Arg("data") data: LoginRequest): Promise<LoginResponse> {
-    return await UserService.login(data);
+  async sendVerificationEmail(
+    @Arg("data") data: SendEmailRequest
+  ): Promise<{ message: string }> {
+    return await EmailService.sendVerificationEmail(data.email, data.nickname);
   }
 }

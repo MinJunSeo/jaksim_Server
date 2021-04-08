5111a367c3cca19820e1336fcbf44b38dc5ec686
MinJunSeo <smj0385@naver.com>
2021-04-08 09:03:37 +0900

    refactor: feature branch

--- src/dto/request/user.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from "type-graphql";
-import { Length } from "class-validator";
-
-@InputType()
-export class UserRequest {
-  @Field()
-  @Length(6, 8)
-  username!: string;
-}

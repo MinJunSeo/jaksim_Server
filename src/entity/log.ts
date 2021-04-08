a751dc8d98acbe20f88609e9b8a51f6aa0b97825
MinJunSeo <smj0385@naver.com>
2021-04-08 09:06:39 +0900

    pref: use enum for log message type

--- src/entity/log.ts
+++ src/entity/log.ts
@@ -1,6 +1,11 @@
 import { Field, ID, ObjectType } from "type-graphql";
 import { User } from "./user";
 
+enum LogType {
+  Login = "LOGIN",
+  Posting = "POSTING"
+}
+
 @ObjectType()
 export class Log {
   @Field((type) => ID)
@@ -9,8 +14,8 @@ export class Log {
   @Field()
   date!: Date;
 
-  @Field()
-  type!: string;
+  @Field((type) => LogType)
+  type!: LogType;
 
   @Field((type) => User)
   user!: User;

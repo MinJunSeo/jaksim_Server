f8aed5f69615e0952d9b8fd830df5847aa6b29b0
MinJunSeo <smj0385@naver.com>
2021-04-09 09:39:43 +0900

    feat: BadRequest response dto

---
+++ src/dto/response/badRequest.ts
@@ -0,0 +1,11 @@
+import { ObjectType, Field } from "type-graphql";
+
+@ObjectType()
+export class BadRequest {
+  constructor() {
+    this.message = "BAD REQUEST";
+  }
+
+  @Field()
+  message: string;  
+}
